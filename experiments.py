da60d61ba728c0adcaff629d8cc9cb9420d4192e
fsvbach <simmiasmobil@gmail.com>
2021-02-07 09:29:33 +0100

    cleaning

--- experiments.py
+++ experiments.py
@@ -11,15 +11,15 @@ import matplotlib.pyplot as plt
 (training_data, test_data) = data.load_data()
 
 
-# mkd = kernels.MismatchKernelDirect(11, 1)
-# K2 = mkd.plot_matrix(train[2], train[2])
-# print(K2)
+mkd = kernels.MismatchKernel(11, 2,1)
+K2 = mkd.plot_matrix(training_data[2], training_data[2])
+print(K2)
 
 
-# best models fo far
-models  = [models.SVM(C) for C in [7,14,8]]
-kernels = [kernels.MismatchKernelDirect(K, M) for (K,M) in zip([10,10,11],[1,1,1])]
+# # best models fo far
+# models  = [models.SVM(C) for C in [7,14,8]]
+# kernels = [kernels.MismatchKernelDirect(K, M) for (K,M) in zip([10,10,11],[1,1,1])]
 
-#save predictions
-util.save_predictions(models, kernels, training_data, test_data)
+# #save predictions
+# util.save_predictions(models, kernels, training_data, test_data)
 
